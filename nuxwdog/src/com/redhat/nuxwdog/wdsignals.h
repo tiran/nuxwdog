a1915a748cd588b058b332980d99677081e52542
mharmsen <mharmsen@ae4d3d4d-437f-0410-b27f-a0f7bb36e700>
2015-07-16 19:32:59 +0000

    Populated PKI_8_1_6_ERRATA_20165_2015052 tag.

--- nuxwdog/src/com/redhat/nuxwdog/wdsignals.h
+++ nuxwdog/src/com/redhat/nuxwdog/wdsignals.h
@@ -32,6 +32,5 @@ void parent_watchdog_create_signal_handlers(void);
 void watchdog_create_signal_handlers(void);
 void watchdog_delete_signal_handlers(void);
 void watchdog_wait_signal(void);
-void disable_sigchld_for_one_signal(void);
 
 #endif /* _WDSIGNAL_H_ */
